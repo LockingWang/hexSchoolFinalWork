b79b5a36d76618a4a9fd1a8a165e3a2e9b6c5d2d
CONG-REN WANG <boom0525300@gmail.com>
2023-02-07 16:21:56 +0800

    Swiper Banner 效果

--- js/all.js
+++ js/all.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+    /* 下拉式選單 */
     $('.product-intro').click(function (e) { 
         e.preventDefault();
         $('.product-list').slideToggle();
@@ -9,4 +10,29 @@ $(document).ready(function () {
         $(this).siblings().find('a:first').removeClass('clickOn');
         $(this).siblings().find('.product-list').slideUp();
     });
+
+    /* 回到頂端按鈕 */
+    $('.goToTop').click(function (e) { 
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: 0,
+        },1000);
+    });
+    $(window).on('scroll',function() {
+        if ( $(this).scrollTop() > 200){
+          $('.goToTop').fadeIn("fast");
+        } else {
+          $('.goToTop').stop().fadeOut("fast");
+        }
+      });
 });
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    autoplay: {
+        delay: 5000,
+      },
+    speed: 1000,
+  });
