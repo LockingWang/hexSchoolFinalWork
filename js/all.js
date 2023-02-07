21fcfbc68944f5106c04015b2d80826b6e211638
CONG-REN WANG <boom0525300@gmail.com>
2023-02-07 13:34:14 +0800

    下拉式選單

---
+++ js/all.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    $('.product-intro').click(function (e) { 
+        e.preventDefault();
+        $('.product-list').slideToggle();
+    });
+    $('.navbar-list>li').click(function (e) { 
+        e.preventDefault();
+        $(this).find('a:first').toggleClass('clickOn');
+        $(this).siblings().find('a:first').removeClass('clickOn');
+    });
+});
